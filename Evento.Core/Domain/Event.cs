6d0ec31ab34c9b85b41597d20f76f111409c4bd5
adambednarz <45689955+adambednarz@users.noreply.github.com>
2019-11-14 11:51:42 +0100

    Creating domain models (DDD)

---
+++ Evento.Core/Domain/Event.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evento.Core.Domain
+{
+    public class Event : Entity
+    {
+        private ISet<Ticket> _tickets = new HashSet<Ticket>();
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime StartDate { get; protected set; }
+        public DateTime EndDate { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+        public IEnumerable<Ticket> Tickets => _tickets;
+        //public int TicketAmount { get; protected set; }
+
+        protected Event()
+        {
+        }
+        public Event(Guid id, string name, string description, 
+            DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void AddTickets(int amount, decimal price)
+        {
+            var seating = _tickets.Count + 1;
+            for (int i = 0; i < amount; i++)
+            {
+                _tickets.Add(new Ticket(this, price, seating));
+                seating++;
+            }
+        }
+    }
+}
---
+++ Evento.Core/Domain/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evento.Core.Domain
+{
+    public class Ticket : Entity
+    {
+        public Guid EventId { get; protected set; }
+        public Guid? UserId { get; protected set; }
+        public string Username { get; protected set; }
+        public DateTime? PurchasedAt { get; protected set; }
+        public decimal Price { get; protected set; }
+        public int Seating { get; protected set; }
+        public bool Purchased => UserId.HasValue;
+
+        protected Ticket()
+        {
+        }
+
+        public Ticket(Event @event, decimal price, int seating)
+        {
+            EventId = @event.Id;
+            Price = price;
+            Seating = seating;
+        }
+
+    }
+}
