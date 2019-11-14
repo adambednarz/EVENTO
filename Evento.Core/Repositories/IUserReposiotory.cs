0d01f27ab6d186501ebe2b59c24113ec0311d48e
adambednarz <45689955+adambednarz@users.noreply.github.com>
2019-11-14 12:16:13 +0100

    Creating Repository interfaces

---
+++ Evento.Core/Repositories/IUserReposiotory.cs
@@ -0,0 +1,18 @@
+﻿using Evento.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evento.Core.Repositories
+{
+    public interface IUserReposiotory
+    {
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(string email);
+        Task<IEnumerable<User>> BrowseAsync(string name = "");
+        Task AddAsync(User user);
+        Task UpdatedAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
