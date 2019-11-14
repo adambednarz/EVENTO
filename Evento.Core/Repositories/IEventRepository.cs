5041bf4f8d1b8e06b572dbe0323c9dc2b812f5a4
adambednarz <45689955+adambednarz@users.noreply.github.com>
2019-11-14 12:52:36 +0100

    Implementation Reposiotories in Infrastructure

--- Evento.Core/Repositories/IEventRepository.cs
+++ Evento.Core/Repositories/IEventRepository.cs
@@ -8,10 +8,10 @@ namespace Evento.Core.Repositories
 {
     public interface IEventRepository
     {
+        Task AddAsync(Event @event);
         Task<Event> GetAsync(Guid id);
         Task<Event> GetAsync(string name);
         Task<IEnumerable<Event>> BrowseAsync(string name = "");
-        Task AddAsync(Event @event);
         Task UpdatedAsync(Event @event);
         Task DeleteAsync(Event @event);
     }
---
+++ Evento.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Evento.Core.Domain;
+using Evento.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evento.Infrastructure.Repositories
+{
+    public class UserRepository : IUserReposiotory
+    {
+        private readonly static ISet<User> _users = new HashSet<User>(); 
+        public async Task AddAsync(User user)
+        {
+            _users.Add(user);
+            await Task.CompletedTask;
+        }
+        public async Task<User> GetAsync(Guid id)
+            => await Task.FromResult(_users.SingleOrDefault(x => x.Id == id));
+
+        public async Task<User> GetAsync(string email)
+            => await Task.FromResult(_users.SingleOrDefault(x => x.Email == email));
+
+
+        public async Task<IEnumerable<User>> BrowseAsync(string name = "")
+        {
+            var users = _users.AsEnumerable();
+            if(!string.IsNullOrWhiteSpace(name))
+            {
+                users = users.Where(x => x.Name.Contains(name));
+            }
+
+            return await Task.FromResult(users);
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _users.Remove(user);
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdatedAsync(User user)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
